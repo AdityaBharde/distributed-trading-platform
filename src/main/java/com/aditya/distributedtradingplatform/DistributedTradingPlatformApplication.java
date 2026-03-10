62479460125ee879cea4d32a5cb04c9014b8f36b
Aditya <adityabharde05@gmail.com>
2026-03-10 18:25:24 +0530

    initial commit - trading platform foundation

---
+++ src/main/java/com/aditya/distributedtradingplatform/DistributedTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.distributedtradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTradingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTradingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aditya/distributedtradingplatform/DistributedTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.distributedtradingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTradingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
